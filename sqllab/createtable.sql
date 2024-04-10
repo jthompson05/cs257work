8887df3ddc5227eb5136e4547e76343d17c96c8b
Joshua Thompson <joshuaandrewt@outlook.com>
2024-04-10 10:56:16 -0500

    modified sql file

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,12 +1,10 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
   quakedate date,
-  quaketime time with time zone,
   latitude real,
   longitude real,
   quakedepth real,
   mag real,
-  id text,
+  rms real,
   place text,
-  quaketype text
 );
