9adafc3931b264776245a18820ad45a7ff8cbb64
Joshua Thompson <joshuaandrewt@outlook.com>
2024-04-24 10:46:53 -0500

    update flask-mega.py

---
+++ flasklab/flask-mega.py
@@ -0,0 +1,30 @@
+import flask
+
+app = flask.Flask(__name__)
+
+#
+@app.route('/hello')
+def my_function():
+    return "Hello World!"
+
+@app.route('/display/<word1>/<word2>')
+def my_display(word1, word2):
+    the_string = "The words are: " + word1 + " and " + word2;
+    return the_string
+
+@app.route('/color/<word1>')
+def my_color(word1):
+    return '<h1 style="color:DodgerBlue">' + word1 + '</h1>'
+
+@app.route('/add/<num1>/<num2>')
+def my_display(num1, num2):
+    sum = int(num1) + int(num2)
+    the_string = "The sum of the two numbers is " + str(sum) + ".";
+    return the_string
+
+
+
+if __name__ == '__main__':
+    my_port = 5128
+    app.run(host='0.0.0.0', port = my_port) 
+    
