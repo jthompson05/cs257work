937703a5078a1e424eb0144d645955d859df6944
jthompson05 <joshuaandrewt@outlook.com>
2024-05-16 19:55:40 -0500

    add files for csslab

---
+++ csslab/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    my_port = 5156
+    app.run(host='0.0.0.0', port = my_port) 
---
+++ csslab/static/script.js
@@ -0,0 +1,7 @@
+function displayAlert() {
+    alert("You've unlocked the magic of the Smiley Lab! 😊");
+}
+
+function changeColor(element) {
+    element.style.color = "blue";
+}
